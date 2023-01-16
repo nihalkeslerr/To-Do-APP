10859d139d13e9f27a163c8d6528fda3724c4018
nihalselmakesler@gmail.com <88909415+nihalkeslerr@users.noreply.github.com>
2023-01-16 22:45:19 +0300

    delete işlevi ve footer kısmı eklendi

--- src/components/ToDo/ListOfGoals/indexList.js
+++ src/components/ToDo/ListOfGoals/indexList.js
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, {useState}from 'react'
 
 
 function List({goals,setGoal}) {
+  const [checked, setChecked]=useState({checked:""})
+
+  const deneme=(e)=>{
+    console.log("onchange")
+  }
+
+  const checkedif=(e)=>{
+    if (e.detail==1) {
+      setGoal(
+        goals.map((item)=>{
+          return !item.completed ? {...item, completed: true} :item 
+          
+      }))
+      setChecked({checked:true})}
+
+    if(e.detail==2){
+      setGoal(
+        goals.map((item)=>{
+          return item.completed ? {...item, completed: false} :item 
+          
+      }))
+      setChecked({checked:false})}
+  }
+
+  const destroy=(note)=>{
+    
+  }
+
   return (
     <section className="main">
-        <input className="toggle-all" type="checkbox" />
-        <label htmlFor="toggle-all" onClick={()=>{
-          setGoal(
-            goals.map((item)=>{
-              return !item.completed ? {...item, completed: true} :item 
-            })
-          )
-        }}>
+
+            <input className="toggle-all" type="checkbox" 
+            checked={checked.checked} onChange={deneme} />
+
+            <label htmlFor="toggle-all" onClick={checkedif}>
 
             Mark all as complete
         </label>
@@ -19,23 +44,28 @@ function List({goals,setGoal}) {
         <ul className="todo-list">
         { 
             goals.map((goal,i,j,k,a,b)=>(
-            <li key={a} className={goal.completed ? "completed" : ""}>
-              <div  key={k} className="view">
 
-                <input key={j} className="toggle" type="checkbox" checked={goal.completed} onChange={()=>{
+            <li key={i} className={goal.completed ? "completed" : ""}>
+              <div key={j} className="view">
+
+                <input key={k} className="toggle" type="checkbox" checked={goal.completed} onChange={()=>{
                   setGoal(
                     goals.map((item)=>{
                       return item===goal ?
                       {...item, completed: !item.completed} :item 
-                    }))}}
-                />
-                <label key={i}>{goal.note} </label>
-                <button key={b} className="destroy"></button>
+                      
+                    }))}}/>
 
-                </div> 
+                <label key={a}>{goal.note} </label>
+                <button key={b} className="destroy" onClick={()=>{
+
+                  const newlist=goals.filter((filt)=>filt.note !==goal.note)
+                  setGoal(newlist)
+                }} ></button>
+
+              </div> 
             </li>
-            ))
-            }
+            ))}
         </ul>
     </section>
         
--- src/components/ToDo/indexGeneral.js
+++ src/components/ToDo/indexGeneral.js
@@ -2,7 +2,7 @@ import React, {useState,useEffect} from 'react'
 import {} from './style.css'
 import IndexAdd from './AddSection/indexAdd'
 import List from './ListOfGoals/indexList';
-
+import Footer from './Footer/indexFoot';
 
 function ToDo() {
   const [goals, setGoal]= useState([]);
@@ -11,6 +11,7 @@ function ToDo() {
 
     console.log("Goals: ",goals);
     
+    
   },[goals])
 
   return (
@@ -18,9 +19,15 @@ function ToDo() {
     <section className='todoapp'>
         <IndexAdd addgoal={setGoal} goals={goals}/>
         <List  setGoal={setGoal} goals={goals}/>
+        <Footer/>
        
 
     </section>
+    <footer className="info">
+        <p>Click to edit a todo</p>
+        <p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
+        <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+    </footer>
     </div>
   )
 }
